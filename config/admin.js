9759954d8d1ec8ee06de78ff611d9b97b2391761
zlatkoflash <zlatkoflash@gmail.com>
2022-07-19 23:42:24 +0200

    Add files via upload

--- config/admin.js
+++ config/admin.js
@@ -1,8 +1,10 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    /*secret: env('ADMIN_JWT_SECRET'),*/
+    secret: '&E)H@McQfTjWnZr4t7w!z%C*F-JaNdRg',
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    /*salt: env('API_TOKEN_SALT'),*/
+    salt: 'y/B?E(H+MbQeThWmZq4t6w9z$C&F)J@N'
   },
 });
--- config/database.js
+++ config/database.js
@@ -1,11 +1,29 @@
 const path = require('path');
 
-module.exports = ({ env }) => ({
+/*module.exports = ({ env }) => ({
   connection: {
     client: 'sqlite',
     connection: {
       filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
     },
     useNullAsDefault: true,
+  },
+});*/
+
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'postgres',
+    connection: {
+      host: env('DATABASE_HOST', 'ec2-54-155-110-181.eu-west-1.compute.amazonaws.com'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'd6saj1llkth6n1'),
+      user: env('DATABASE_USERNAME', 'hzbreewutetpqp'),
+      password: env('DATABASE_PASSWORD', '90e52511c988bcf31d5d08f5adcd02caade703edd41eb70bbcebe439d852bd33'),
+      schema: env('DATABASE_SCHEMA', 'public'), // Not required
+      ssl: {
+        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
+      },
+    },
+    debug: false,
   },
 });
